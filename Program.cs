d8bceff8d1d690700239fadf4a86a3c761eafc6e
314918200 <31561831+314918200@users.noreply.github.com>
2017-09-02 22:32:14 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ss = "sakjfgldskajfk     sfasd gdsfhg   fghdsh    ghfghfa siaewri df   gdsfgksdlfgk          ldskgsl  ld   glfdkg   lfkdsgjlfs ll  lk  j jdj  glskg   ;fglhs h ssgs ;sk j  ";
+            string[] gg =ss.Split(new char[] { },StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in gg)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("****************************************");
+                Console.ReadKey();
+            }
+        }
+    }
+}
